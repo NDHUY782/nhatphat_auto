d085d5648c337ad5d5339766ec9c353de1b6d99c
Duc Huy <huynguyen07080401@gmail.com>
2025-06-14 20:58:29 +0700

    fix: fix [] for services

--- src/models/requests/PriceService.request.ts
+++ src/models/requests/PriceService.request.ts
@@ -2,15 +2,15 @@ import { ObjectId } from 'mongodb'
 
 export interface CreatePriceServiceRequestBody {
   name_service: string
-  vehicle_type: string
-  unit: string
-  price: string
+  vehicle_type: string[]
+  unit: string[]
+  price: string[]
 }
 
 export interface UpdatePriceServiceRequestBody {
   name_service?: string
-  vehicle_type?: string
-  unit?: string
-  price?: string
+  vehicle_type?: string[]
+  unit?: string[]
+  price?: string[]
   updated_at?: Date
 }
--- src/models/schemas/PriceOfService.Schema.ts
+++ src/models/schemas/PriceOfService.Schema.ts
@@ -3,9 +3,9 @@ import { ObjectId } from 'mongodb'
 export interface PriceServiceType {
   _id?: ObjectId
   name_service: string
-  vehicle_type: string
-  unit: string
-  price: string
+  vehicle_type: string[]
+  unit: string[]
+  price: string[]
   created_at?: Date
   updated_at?: Date
 }
@@ -13,18 +13,18 @@ export interface PriceServiceType {
 export default class PriceService {
   _id?: ObjectId
   name_service: string
-  vehicle_type: string
-  unit: string
-  price: string
+  vehicle_type: string[]
+  unit: string[]
+  price: string[]
   created_at?: Date
   updated_at?: Date
 
   constructor({ _id, name_service, vehicle_type, unit, price, created_at, updated_at }: PriceServiceType) {
     this._id = _id || new ObjectId()
     this.name_service = name_service
-    this.vehicle_type = vehicle_type
-    this.unit = unit
-    this.price = price
+    this.vehicle_type = vehicle_type || []
+    this.unit = unit || []
+    this.price = price || []
     this.created_at = created_at || new Date()
     this.updated_at = updated_at || new Date()
   }
